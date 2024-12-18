9bffa7b690f3346f50de13c44a625beb45868f61
남화성 <hsnam@gaonpf.com>
2024-12-18 06:37:54 +0000

    cuda learn

---
+++ cuda_learn/07b-cpu-kernel.cu
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <cuda.h>
+#include <cuda_runtime_api.h>
+#include <cuda_runtime.h>
+
+void add_kernel( int idx, const int* a, const int* b, int* c ) {
+    int i = idx;
+    c[i] = a[i] + b[i];
+}
+
+// main program for the CPU: compiled by MS-VC++
+int main(void) {
+	// host-side data
+	const int SIZE = 5;
+	const int a[SIZE] = { 1, 2, 3, 4, 5 };
+	const int b[SIZE] = { 10, 20, 30, 40, 50 };
+	int c[SIZE] = { 0 };
+	// calculate the addition
+	for (register int i = 0; i < SIZE; ++i) {
+		add_kernel( i, a, b, c );
+	}
+	// print the result
+	printf("{%d,%d,%d,%d,%d} + {%d,%d,%d,%d,%d} = {%d,%d,%d,%d,%d}\n",
+	       a[0], a[1], a[2], a[3], a[4],
+	       b[0], b[1], b[2], b[3], b[4],
+	       c[0], c[1], c[2], c[3], c[4]);
+	fflush( stdout );
+	// done
+	return 0;
+}
+
+/* (c) 2021-2022. biztripcru@gmail.com. All rights reserved. */
